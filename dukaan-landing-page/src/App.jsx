dc8bf6eddc620f53c2165f75648af251e58b2ab0
ArjunHurket <arjunhurkat@gmail.com>
2024-08-05 16:20:13 +0530

    Project Setup

---
+++ dukaan-landing-page/src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+    <div className="grid grid-cols-12">
+      <div className="col-span-12 md:col-span-4 bg-red-500">1</div>
+      <div className="col-span-12 md:col-span-4 bg-yellow-500">Abc</div>
+      <div className="col-span-12 md:col-span-4 bg-green-500">Hdefg</div>
+    </div>
+    
+        
+    </>
+  )
+}
+
+export default App
